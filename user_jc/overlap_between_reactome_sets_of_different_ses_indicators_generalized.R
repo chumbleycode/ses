14b0ff6438d85ff3f46d3c1eb40290caf7c43667
Chumbley <chumbley@jccompute.uzh.ch>
2021-01-27 17:10:02 +0100

    generalize functional venn-

--- user_jc/overlap_between_reactome_sets_of_different_ses_indicators_generalized.R
+++ user_jc/overlap_between_reactome_sets_of_different_ses_indicators_generalized.R
@@ -32,8 +32,8 @@ get_venn_cell =
     
     # Logic: each venn cell has form ABCDE (interpreted as set intersection or
     # product of set characteristic functions). The collection of 2^D (here 2^5)
-    # such sets -  where every factor of ABCDE or it's a compliment are permitted
-    # independently - fill the 2^D cells of the Ven diagram.
+    # such set intersections -  where every factor of ABCDE or it's a compliment
+    # are permitted independently - fill the 2^D cells of the Ven diagram.
     
     # for each row in matrix of indexes P
     map(1:dim(P)[1], 
